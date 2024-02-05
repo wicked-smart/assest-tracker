303a82b1f0cf165f9334614ea03171993c92803e
Prem Kumar <kumar8777prem@gmail.com>
2024-02-05 10:22:48 +0530

    Add feature of adding console logs to a log gile and also add rotation feature whenever log file exceeds certain size

--- asset_tracker/asset_tracker_middleware.py
+++ asset_tracker/asset_tracker_middleware.py
@@ -1,12 +1,44 @@
-
+import os
 from django.urls import resolve
+from datetime import datetime
 
 
 
 class AssetTrackerMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
+        self.log_path = 'asset_tracker.log'
+        self.max_file_size = 2000  # Maximum file size in bytes
+        self.setup_logging()
 
+    def setup_logging(self):
+        #  if the log file exists and exceeds the maximum size
+        if os.path.exists(self.log_path) and os.path.getsize(self.log_path) > self.max_file_size:
+             
+            #create a new log file
+            base_name, extension = os.path.splitext(self.log_path)
+            timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
+            new_log_path = f'{base_name}_{timestamp}{extension}'
+            self.log_path = new_log_path
+            
+            with open(new_log_path, 'a') as log_file:
+                pass 
+
+    def log_message(self, message):
+        # Check if the log file exceeds the maximum size
+        if os.path.getsize(self.log_path) > self.max_file_size:
+            self.setup_logging()  
+
+        # Add the current date to the log message
+        timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        log_message = f"{timestamp} - {message}\n"
+
+        # Append the log message to the file
+        with open(self.log_path, 'a') as log_file:
+            log_file.write(log_message)
+
+        
+    
     def __call__(self, request):
 
         #print("middleware is running ")
@@ -22,33 +54,34 @@ class AssetTrackerMiddleware:
 
 
             if view_name.__name__ == "asset_types":
-               print("Asset Tracker MiddleWare - Asset types Listed!")
+               self.log_message("Asset Tracker MiddleWare - Asset types Listed!")
+            
             if view_name.__name__ == "index":
-               print("Asset Tracker MiddleWare - Dashboard Page Loaded!")
+               self.log_message("Asset Tracker MiddleWare - Dashboard Page Loaded!")
             if view_name.__name__ == "asset_types_detail":
-               print("Asset Tracker MiddleWare - Asset types detail Loaded!")
+               self.log_message("Asset Tracker MiddleWare - Asset types detail Loaded!")
             if view_name.__name__ == "asset_type_update":
-               print("Asset Tracker MiddleWare - Asset types Updated!")
+               self.log_message("Asset Tracker MiddleWare - Asset types Updated!")
             if view_name.__name__ == "asset_type_add":
-               print("Asset Tracker MiddleWare - New Asset type Added!")
+               self.log_message("Asset Tracker MiddleWare - New Asset type Added!")
             if view_name.__name__ == "asset_type_delete":
-               print("Asset Tracker MiddleWare -  Asset type Deleted!")
+               self.log_message("Asset Tracker MiddleWare -  Asset type Deleted!")
 
             if view_name.__name__ == "asset_add":
-               print("Asset Tracker MiddleWare - New Asset Added!")
+               self.log_message("Asset Tracker MiddleWare - New Asset Added!")
             if view_name.__name__ == "assets":
-               print("Asset Tracker MiddleWare - Assets Listed!")
+               self.log_message("Asset Tracker MiddleWare - Assets Listed!")
             if view_name.__name__ == "asset_detail":
-               print("Asset Tracker MiddleWare - Asset Detail Listed!")
+               self.log_message("Asset Tracker MiddleWare - Asset Detail Listed!")
             if view_name.__name__ == "asset_update":
-               print("Asset Tracker MiddleWare - Asset Updated!")
+               self.log_message("Asset Tracker MiddleWare - Asset Updated!")
             if view_name.__name__ == "asset_delete":
-               print("Asset Tracker MiddleWare - Asset Deleted!")
+               self.log_message("Asset Tracker MiddleWare - Asset Deleted!")
             if view_name.__name__ == "generate_csv":
-               print("Asset Tracker MiddleWare - Assets CSV File downloaded !")
+              self.log_message("Asset Tracker MiddleWare - Assets CSV File downloaded !")
 
         except Exception as e:
-            print(f"Asset Tracker MiddleWare -Unable to resolve the view name: {e}!")
+            self.log_message(f"Asset Tracker MiddleWare -Unable to resolve the view name: {e}!")
         
         response = self.get_response(request)
 
